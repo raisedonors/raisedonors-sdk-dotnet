d472b202f4b53f7b730026dc096559a5c3e189da
Chad Meyer <chadmeyer@me.com>
2018-05-06 23:42:29 -0500

    Adding a way to get and save product variants

---
+++ src/RaiseDonors.Rest/Store/Models/ProductVariant.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaiseDonors.Rest.Store.Models {
+    public class ProductVariant {
+        public long ID { get; set; }
+
+        public long ProductID { get; set; }
+
+        public int ProductFormatID { get; set; }
+
+        public string SKU { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public bool DisplaySKU { get; set; }
+
+        public bool Downloadable { get; set; }
+
+        public string DownloadUrl { get; set; }
+
+        public bool MultipleQuantities { get; set; }
+
+        public decimal? AdditionalCopyCost { get; set; }
+
+        public int? AdditionalCopyMax { get; set; }
+
+        public decimal FairMarketValue { get; set; }
+
+        public decimal? USMinDonation { get; set; }
+
+        public decimal? UKMinDonation { get; set; }
+
+        public decimal? CanadaMinDonation { get; set; }
+
+        public decimal? InternationalMinDonation { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public string LastModifiedBy { get; set; }
+    }
+}
