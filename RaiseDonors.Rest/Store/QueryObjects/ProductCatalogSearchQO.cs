eb3ed276ff4c89ae7cc9dd49dbb76829d01f0d20
Chad Meyer <chadmeyer@52projectsllc.com>
2018-08-07 12:58:35 -0500

    Adding a way to search the product catalog for products

---
+++ RaiseDonors.Rest/Store/QueryObjects/ProductCatalogSearchQO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using RaiseDonors.Rest.Attributes;
+using RaiseDonors.Rest.QueryObjects;
+
+namespace RaiseDonors.Rest.Store.QueryObjects {
+    public class ProductCatalogSearchQO : BaseQO {
+
+        [QO("topic")]
+        public long? TopicID { get; set; }
+
+        [QO("format")]
+        public long? Format { get; set; }
+
+        [QO("search")]
+        public string SearchText { get; set; }
+    }
+}
